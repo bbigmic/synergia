aeb27bd0febb012e3afe4709b75bdd9f2c4dba75
bbigmic <122184799+bbigmic@users.noreply.github.com>
2025-12-29 20:36:20 +0100

    update stripe version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export const WEEKLY_PRICE_ID = process.env.STRIPE_WEEKLY_PRICE_ID || "";
